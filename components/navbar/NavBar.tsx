964e259ecd5651a590e9b2d73914706ea98d5507
jmoraba_bci <jhan.mora@bci.cl>
2023-01-10 22:03:20 -0300

    Add: Ajusto la modificacion del contrato en storybook

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { MenuItem } from "./MenuItem";
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 import about from "../../public/about.png";
 import paints from "../../public/paints.png";
 import accesories from "../../public/accesories.png";
@@ -8,26 +8,10 @@ import blogs from "../../public/blogs.png";
 export const NavBar = () => {
   return (
     <nav className={styles.navigationContainer}>
-      <MenuItem
-        logo={about}
-        title="Home"
-        link="/"
-      />
-      <MenuItem
-        logo={paints}
-        title="Home"
-        link="/"
-      />
-      <MenuItem
-        logo={accesories}
-        title="Home"
-        link="/"
-      />
-      <MenuItem
-        logo={blogs}
-        title="Home"
-        link="/"
-      />
+      <MenuItem logo={about} title="Home" link="/" />
+      <MenuItem logo={paints} title="Paints" link="/" />
+      <MenuItem logo={accesories} title="Accesories" link="/" />
+      <MenuItem logo={blogs} title="Blogs" link="/" />
     </nav>
   );
 };
---
+++ stories/MenuItem.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MenuItem } from "../components/navbar/MenuItem";
+import about from "../public/about.png";
+
+export default {
+  title: "NavBar/MenuItem",
+  component: MenuItem,
+  argTypes: {
+    logo: { control: "text" },
+    title: { control: "text" },
+    link: { control: "text" },
+  },
+} as ComponentMeta<typeof MenuItem>;
+
+const Template: ComponentStory<typeof MenuItem> = (args) => (
+  <MenuItem {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  logo: about,
+  title: "Title",
+  link: "/",
+};
