e74b6725f9232408a80dc2411c02d6ce3465af04
jmoraba_bci <jhan.mora@bci.cl>
2023-01-12 19:40:09 -0300

    Feat: añado api para obtener las categorias

---
+++ constants/category.ts
@@ -0,0 +1,32 @@
+export const CATEGORY = [
+  {
+    id: 1,
+    name: "All",
+    type: "paints",
+  },
+  {
+    id: 2,
+    name: "Oil",
+    type: "paints",
+  },
+  {
+    id: 3,
+    name: "Acrylic",
+    type: "paints",
+  },
+  {
+    id: 4,
+    name: "Watercolor",
+    type: "paints",
+  },
+  {
+    id: 4,
+    name: "Watercolor",
+    type: "paints",
+  },
+  {
+    id: 5,
+    name: "other",
+    type: "accesories",
+  },
+];
---
+++ pages/api/category/[type].ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CATEGORY } from "../../../constants";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { type } = req.query;
+  const isValidCategory = CATEGORY.some((category) => category.type === type);
+  if (!isValidCategory)
+    return res.status(404).json({ message: "Not found", status: 404 });
+
+  res
+    .status(200)
+    .json(CATEGORY.filter((category) => category.type === req.query.type));
+}
